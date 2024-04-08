7672f26fd6c26d5691b92164f9f2194eb2296c7c
Sebastian Hernandez (from Dev Box) <sebhernandez@microsoft.com>
2024-04-08 06:10:27 -0700

    draft pr feature

--- Extensions/Program.cs
+++ Extensions/Program.cs
@@ -5,7 +5,7 @@
 /// When using an extension method to extend a type whose source code you aren't in control of, you run the risk that a change in the implementation of the type will cause your extension method to break.
 /// 
 /// 
-/// </summary> change
+/// </summary> change new draft pr
 public interface FooInterface
 {
     string Name { get; set; }
