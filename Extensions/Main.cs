5f35fa3283af2aef4e848c567f52c9976f928ed0
Sebastian Hernandez <sebaoctavio@gmail.com>
2025-02-25 07:25:50 -0800

    this is my bubble sort

--- Extensions/Main.cs
+++ Extensions/Main.cs
@@ -1,18 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-// added comment
-// remote change
-namespace Extensions
+class Program
 {
-    internal class Main
+    static void Main()
     {
-        // main class
-        // local change
-        //  change
-        // related intem
+        int[] arr = { 5, 2, 9, 1, 5, 6 };
+        BubbleSort(arr);
+
+        Console.WriteLine("Sorted array: ");
+        foreach (var item in arr)
+        {
+            Console.Write(item + " ");
+        }
+    }
+
+    static void BubbleSort(int[] arr)
+    {
+        // Incorrect loop condition - will not sort the array fully
+        for (int i = 0; i < arr.Length; i++) // Should iterate less for each pass
+        {
+            for (int j = 0; j < arr.Length - 1; j++) // This loop is incorrect since it doesn't reduce the range after each pass
+            {
+                if (arr[j] > arr[j + 1]) // Swap if the element is greater than the next one
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
 }
